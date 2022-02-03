8df811f9f79c3e9c3344d094249e04865899ff74
ludo12h10 <78366594+ludo12h10@users.noreply.github.com>
2022-02-03 17:31:10 +0100

    Premier commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+function Header() {
+    return (<h1>La maison jungle</h1>)
+}
+
+function Description(){
+    const text = "Ici achetez toutes les plantes dont vous avez toujours rêvé 🌵";
+    const emoji = "😀👏👍"
+    return (<p>{text + emoji}</p>)
+}
+
+function Banner(){
+    return(
+        <React.Fragment>
+            <Header/>
+            <Description/>
+        </React.Fragment>
+    )
+}
+
+ReactDOM.render(<Banner />, document.getElementById("root"))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
