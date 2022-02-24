040ecaba2bff048faa0f88d52af9e6a16ca31928
ludo12h10 <78366594+ludo12h10@users.noreply.github.com>
2022-02-24 09:59:13 +0100

    2022-02-24

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.css';
 import Banner from "./composants/Banner";
 import React from "react";
 import ShoppingList from "./composants/ShoppingList";
+import Cart from './composants/Cart';
+
 import QuestionForm from "./composants/QuestionForm";
 
 function App() {
     return (
-        <React.Fragment>
-            {/*<Banner/>*/}
+        <div>
             <Banner>
-                <img src={logo} alt="la maison jungle" className='lmj-logo'/>
-                <h1 className='lmj-title'> La maison jungle </h1>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
             </Banner>
-            <ShoppingList/>
-            <QuestionForm/>
-        </React.Fragment>
-    );
+            <div className='lmj-layout-inner'>
+                <Cart />
+                <ShoppingList />
+            </div>
+        </div>
+    )
 }
 
 export default App;
